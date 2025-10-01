b4174c3d00ff3a57abcc1da41891d31a6e3dc4dc
gauravpatil24 <99814974+gauravpatil24@users.noreply.github.com>
2025-10-01 21:05:31 +0530

    Add files via upload

---
+++ Using Modules/VM_Windows/output.tf
@@ -0,0 +1,15 @@
+output "ip_value" {
+  value = module.vm.vm_ip
+}
+
+output "virtual_machine_name" {
+  value = module.vm.virtual_machine_name
+}
+
+output "resource_group_name" {
+  value = module.vm.resource_group_name
+}
+
+output "sku" {
+  value = module.vm.sku
+}
