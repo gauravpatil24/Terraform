9390927a84ef0916ac925dfadf94238a047b57f7
gauravpatil24 <99814974+gauravpatil24@users.noreply.github.com>
2025-10-17 14:35:03 +0530

    Add files via upload

---
+++ Using Keyvault/main.tf
@@ -0,0 +1,12 @@
+module "vm" {
+  source                       = "./vm_module"
+  resource_group_name          = var.resource_group_name
+  virtual_network_name         = var.virtual_network_name
+  subnet_name                  = var.subnet_name
+  public_ip_name               = var.public_ip_name
+  virtual_machine_name         = var.virtual_machine_name
+  resource_group_location      = var.resource_group_location
+  keyvault_id                  = var.keyvault_id
+  keyvault_resource_group_name = var.keyvault_resource_group_name
+  keyvault_name                = var.keyvault_name
+}
