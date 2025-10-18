24ce9ab089d690128212949921c06fa05f331507
gauravpatil24 <99814974+gauravpatil24@users.noreply.github.com>
2025-10-18 08:46:55 +0530

    Add files via upload

---
+++ Using Keyvault/backend.tf
@@ -0,0 +1,8 @@
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name = "resgrpstate" 
+#     storage_account_name = "mystorage545454"  
+#     container_name = "container123"
+#     key = "tfstate" 
+# }
+# }
