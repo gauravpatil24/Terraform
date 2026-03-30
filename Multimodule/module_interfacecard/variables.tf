d40bb1331a537c6996c7bf27e19189da729bc66f
gauravpatil24 <99814974+gauravpatil24@users.noreply.github.com>
2026-03-30 20:40:22 +0530

    Add files via upload

---
+++ Multimodule/module_interfacecard/variables.tf
@@ -0,0 +1,19 @@
+variable "nic_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resgrp_name" {
+  type = string
+}
+
+variable "app_subnetid" {
+  type = string
+}
+
+variable "pubip_id" {
+  type = string
+}
