e62b3386b21c4ee3f5f58abc2e2eb2af90d1f450
gauravpatil24 <99814974+gauravpatil24@users.noreply.github.com>
2026-04-16 17:23:41 +0530

    Add files via upload

---
+++ Multimodule/module_network/variables.tf
@@ -0,0 +1,19 @@
+variable "vnet_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resgrp_name" {
+  type = string
+}
+
+variable "app_subnet" {
+  type = string
+}
+
+variable "db_subnet" {
+  type = string
+}
